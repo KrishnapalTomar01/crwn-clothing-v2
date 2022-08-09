13b4397ebfc17b6ad7b023a01a4a451130cd6de9
Krishnapal Tomar <krishnapal.01tomar@gmail.com>
2022-08-09 19:31:26 +0530

    [Lesson 16] Add items to cart

--- src/context/cart.context.jsx
+++ src/context/cart.context.jsx
@@ -1,13 +1,38 @@
 import { createContext, useState } from "react";
 
+const addCartItem = (cartItems, productToAdd) => {
+    //find if cart item contains productToAdd
+
+    const existingCartItem = cartItems.find(cartItem => cartItem.id === productToAdd.id);
+    if(existingCartItem){
+        return cartItems.map(cartItem => 
+            cartItem.id === productToAdd.id ? 
+            {...cartItem, quantity : cartItem.quantity + 1} 
+            : cartItem
+        )
+    }
+    
+    //If found increment quantity
+    return [...cartItems, {...productToAdd, quantity : 1 }]
+    //return new array with modified / new cart item
+}
+
 export const CartContext = createContext({
     cartOpen: false,
-    setCartOpen: () => null
+    setCartOpen: () => null,
+    cartItems :[],
+    addItemToCart: () => null
 });
 
 export const CartProvider = ({ children }) => {
     const [cartOpen, setCartOpen] = useState(false);
-    const value = { cartOpen, setCartOpen };
+    const [cartItems, setCartItems] = useState([]);
+
+    const addItemToCart = (productToAdd) => {
+        setCartItems(addCartItem(cartItems, productToAdd));
+    }
+
+    const value = { cartOpen, setCartOpen, addItemToCart, cartItems };
 
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>
 }
