fb04499ae9417b5de11cf37483662324bdb00a90
Krishnapal Tomar <krishnapal.01tomar@gmail.com>
2022-08-11 11:48:46 +0530

    [Lesson 22] styled components add in navigation

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,6 +1,6 @@
 import { Fragment, useContext } from "react";
-import { Outlet, Link } from "react-router-dom";
-import './navigation.styles.scss';
+import { Outlet } from "react-router-dom";
+import { LogoContainer, NavLink, NavLinksContainer, NavigationContainer } from './navigation.styles.jsx';
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import { UserContext } from "../../context/user.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
@@ -14,23 +14,23 @@ const Navigation = () => {
 
     return (
         <Fragment>
-            <div className="navigation">
-                <Link to='/' className="logo-container">
+            <NavigationContainer>
+                <LogoContainer to='/'>
                     <CrwnLogo className="logo" />
-                </Link>
-                <div className="nav-links-container">
-                    <Link to='/shop' className="nav-link">SHOP</Link>
+                </LogoContainer>
+                <NavLinksContainer>
+                    <NavLink to='/shop'>SHOP</NavLink>
                     {
-                        currentUser ? (<span className="nav-link" onClick={signOutUser}>SIGN OUT</span>) :
-                            (<Link to='/auth' className="nav-link">SIGN IN</Link>)
+                        currentUser ? (<NavLink as='span' onClick={signOutUser}>SIGN OUT</NavLink>) :
+                            (<NavLink to='/auth'>SIGN IN</NavLink>)
                     }
                     <CartIcon />
-                </div>
+                </NavLinksContainer>
                 {
                     cartOpen && <CartDropdown />
                 }
 
-            </div>
+            </NavigationContainer>
             <Outlet />
         </Fragment>
     );
